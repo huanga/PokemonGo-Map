6b911efe7bc07406013e9d798aff473fdaef0ca9
Andy Huang <ahuang@bbtv-8nrce-2.local>
2016-08-07 22:03:17 -0700

    Syntax error, missing closing )

--- runserver.py
+++ runserver.py
@@ -92,7 +92,7 @@ if __name__ == '__main__':
     res = prog.match(args.location)
     if res:
         log.debug('Using coords from CLI directly')
-        position = (float(res.group(1)), float(res.group(2)), float(res.group(3))
+        position = (float(res.group(1)), float(res.group(2)), float(res.group(3)))
     else:
         log.debug('Lookig up coords in API')
         position = util.get_pos_by_name(args.location)
